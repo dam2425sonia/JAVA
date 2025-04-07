80600f6ec58cf05c457a4b40838127b15a810e12
Sonia D. <dam2425sonia@gmail.com>
2025-04-07 12:03:06 +0200

    Update Conexion.java

--- ReportesBBDD/src/Conexion.java
+++ ReportesBBDD/src/Conexion.java
@@ -3,10 +3,10 @@ import java.sql.DriverManager;
 
 public class Conexion {
     public static Connection getConnection() {
-        String url = "jdbc:mysql://185.101.225.45:3306/a7rpj4xs_dam";
-        String user = "a7rpj4xs_dam";
-        String password = "bXAQ95)JNmlj";
-        String dataBase="a7rpj4xs_dam";
+        String url = "jdbc:mysql://185.101.225.45:3306/aquitubbdd";
+        String user = "aquituusuario";
+        String password = "aquitucontraseña";
+
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
